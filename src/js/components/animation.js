39074914f8095491a5c7387227573bce69e40f8b
tomy.dev <xxx.d1231@gmail.com>
2024-10-06 23:42:26 +0900

    不要script削除、templatesファイル名変更

--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -1,10 +1,5 @@
 function animation() {
   console.log("animation loaded OK!");
-  const toggleButton = document.querySelector(".js-toggle");
-
-  toggleButton.addEventListener("click", () => {
-    toggleButton.style.color = "red";
-  });
 }
 
 export default animation;
